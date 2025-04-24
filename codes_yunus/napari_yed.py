1b717ffbf1e6f2451a779d6aa95eccfb0be51310
yunus <yedemir001@gmail.com>
2025-04-24 16:46:59 +0200

    logbook  added.

--- codes_yunus/napari_yed.py
+++ codes_yunus/napari_yed.py
@@ -2,54 +2,108 @@ import matplotlib.pyplot as plt
 import numpy as np
 from skimage import filters, morphology
 from aicsimageio import AICSImage
-file_name = f"/Users/demir/Documents/Hva_AI/afstudeerproject/Images Carolina/20240312_CKR_Exp35_STHdhQ97HA_96h_+BafA_CCT1_635P_HA_580_A11_460L_2.lif"
+file_name = f"/Users/demir/Documents/Hva_AI/afstudeerproject/Images Carolina/20240312_CKR_Exp35_STHdhQ97HA_96h_+BafA_CCT1_635P_HA_580_A11_460L_4.lif"
 
-img = AICSImage(file_name)
-c2 = img.get_image_data("CZYX", T=0, C=2)  # Assuming channel 2 is C2
-
-
-if c2.ndim == 4:
-    c2 = np.squeeze(c2)  # otomatik olarak boyutu düşürür
-    print("c2.shape:", c2.shape)
-
-# Normalize and threshold
-normalized = (c2 - c2.min()) / (c2.max() - c2.min())
-threshold = filters.threshold_otsu(normalized)
-binary = normalized > threshold
-
-# Clean
-binary = morphology.remove_small_objects(binary, min_size=50)
+# img = AICSImage(file_name)
+# c2 = img.get_image_data("CZYX", T=0, C=2)  # Assuming channel 2 is C2
 
 
-from skimage.measure import label, regionprops_table
-import pandas as pd
+# if c2.ndim == 4:
+#     c2 = np.squeeze(c2)  # otomatik olarak boyutu düşürür
+#     print("c2.shape:", c2.shape)
 
-labeled = label(binary)
-props = regionprops_table(
-    labeled, intensity_image=c2,
-    properties=['label', 'area', 'centroid', 'mean_intensity', 'bbox']
-)
+# # Normalize and threshold
+# normalized = (c2 - c2.min()) / (c2.max() - c2.min())
+# threshold = filters.threshold_otsu(normalized)
+# binary = normalized > threshold
 
-df = pd.DataFrame(props)
-df["volume_voxels"] = df["area"]
+# # Clean
+# binary = morphology.remove_small_objects(binary, min_size=50)
 
 
-c1 = img.get_image_data("CZYX", T=0, C=1)
-c1 = np.squeeze(c1)
-from scipy.stats import pearsonr
+# from skimage.measure import label, regionprops_table
+# import pandas as pd
 
-# Flatten both channels
-mask = (binary > 0)
-pearson_corr = pearsonr(c1[mask].flatten(), c2[mask].flatten())[0]
+# labeled = label(binary)
+# props = regionprops_table(
+#     labeled, intensity_image=c2,
+#     properties=['label', 'area', 'centroid', 'mean_intensity', 'bbox']
+# )
 
-print(f"Pearson correlation in colocalized region: {pearson_corr:.3f}")
+# df = pd.DataFrame(props)
+# df["volume_voxels"] = df["area"]
 
+
+# c1 = img.get_image_data("CZYX", T=0, C=1)
+# c1 = np.squeeze(c1)
+# from scipy.stats import pearsonr
+
+# # Flatten both channels
+# mask = (binary > 0)
+# pearson_corr = pearsonr(c1[mask].flatten(), c2[mask].flatten())[0]
+
+# print(f"Pearson correlation in colocalized region: {pearson_corr:.3f}")
+
+# import napari
+
+# viewer = napari.Viewer()
+
+# viewer.add_image(c2, name="C2 - Aggregates")
+# viewer.add_labels(labeled, name="Aggregates")
+# viewer.add_image(c1, name="C1 - Other Channel")  # optional
+# napari.run()  # GUI kontrolü biter, kullanıcı kapatınca devam eder
+
+# --------------------
+import numpy as np
+# from aicsimageio import AICSImage
+# import napari
+
+# # Dosya yolu
+# img = AICSImage(file_name)
+
+# # Tüm kanalları çek (C, Z, Y, X)
+# data = img.get_image_data("CZYX", T=0)
+
+# print("Tüm görüntü şekli (C, Z, Y, X):", data.shape)
+
+# # Napari ile tüm kanalları görselleştir
+# viewer = napari.Viewer()
+
+# channel_names = ["C0 - Nucleus", "C1 - A11", "C2 - HA", "C3 - CCT1"]
+
+# for c in range(data.shape[0]):
+#     viewer.add_image(data[c], name=channel_names[c] if c < len(channel_names) else f"Channel {c}")
+
+# napari.run()
+
+
+# ====
+
+from aicsimageio import AICSImage
 import napari
 
+
+img = AICSImage(file_name)
+
+# Veriyi (T, C, Z, Y, X) olarak al
+data = img.get_image_data("TCZYX")
+
+print("data.shape:", data.shape)
+
+# Napari viewer
 viewer = napari.Viewer()
 
-viewer.add_image(c2, name="C2 - Aggregates")
-viewer.add_labels(labeled, name="Aggregates")
-viewer.add_image(c1, name="C1 - Other Channel")  # optional
-napari.run()  # GUI kontrolü biter, kullanıcı kapatınca devam eder
+# T boyutunu atarak sadece bir zaman dilimi kullan
+data = data[0]  # shape: (C, Z, Y, X)
+channel_names = ["C0 - Nucleus", "C1 - A11", "C2 - HA", "C3 - CCT1"]
+
+# Her kanalı ayrı ayrı göster
+for c in range(data.shape[0]):
+    viewer.add_image(
+        data[c],  # (Z, Y, X)
+        name=channel_names[c] if c < len(channel_names) else f"Channel {c}"
+    )
+
+
+napari.run()
 
--- logbook.md
+++ logbook.md
@@ -32,11 +32,48 @@ However, cell detection is currently not working well. I will focus on it.  Why 
 Until now, I’ve been collapsing the 3D data into a 2D image for easier processing. Do you think this is a valid method, or are there better alternatives for working with 3D data directly?
 In the attached PDF file, I worked with a .lif image and tried to identify protein aggregates. You can ignore the code sections — the photos are the most important part.  Do you think the last image reflects the main goal of the project?
 ```
+# literatuur reserch 
 
-# 23.04.2025 todo list
+
+# Todo list
+## 23.04.2025 
 1. Read expert workshop slides 
 2. Complete Introduction 
-3. compelete Background 
+3. Compelete Background 
 4. Mension about paper which you have read and reseach which you have done with lamia and self in the logbook, so hier.
 
+### Proggess
+* Introduction and background part has been wrote. It looks like compleete. I have got a mail from carolina about the answer of my question and it is more clear now. The project must be 3d image progessing. And the name of the paper and main question eddited. 
 
+* The order of the Z-Stacks is not true in the python. I have been bussy wiht this error for 4 days i coudnt solve it yet. 
+
+
+
+## 24.04.2025
+1. **Requiremernts:** Look at the lessons notes and create and requirements.
+2. **Prototype:** Look at the lessons notes. And bring something to requirements.
+3. 
+
+### Requirements Table
+
+| ID  | Domain        | Type           | Requirement Statement                                                                 | Justification                                                                 | MoSCoW |
+|-----|---------------|----------------|----------------------------------------------------------------------------------------|--------------------------------------------------------------------------------|--------|
+| R1  | Image Analysis | Functional     | The system **must** detect all individual cells in the microscopy images.              | Accurate cell detection is essential to locate and quantify neural aggregates. | Must   |
+| R2  | Image Analysis | Functional     | The system **must** process the HA and CCT1 channel  of the microscopy data.                | HA channel  specifically marks the Huntington-related aggregates. CCT1 channel is usefull to see cell body.         | Must   |
+| R3  | Preprocessing  | Functional     | The system **should** apply noise reduction before segmentation.                       | Reducing noise helps prevent false detections in cell boundaries.              | Should |
+| R4  | Aggregates     | Functional     | The system **must** quantify aggregates larger than a defined intensity threshold.     | Only aggregates above the threshold are considered relevant. | Must   |
+| R5  | Output         | Non-functional | The output **should** include a CSV file with cell positions, aggregate count, and size.| Allows easy further analysis and validation by domain experts.                | Should |
+| R6  | I/O Format     | Functional     | The system **must** accept only `.lif` files as input for 3D microscopy analysis.      | Ensures compatibility with standardized imaging data used in this project.     | Must   |
+| R7  | Output         | Functional     | The system **must** generate a structured file (CSV or JSON) with aggregate count, location, and their association with detected cells. | Provides interpretable results for downstream analysis and biological insight. | Must   |
+| R8  | Output         | -----          | The system **could** generate annotated image projections for visual inspection.       | Helps researchers visually verify the detection quality and segmentation.       | Could  |
+| R9  | System	Compatibility         | ---          | The system should be compatible with Windows, macOS, and Linux for broader accessibility.       | Makes the tool available to a wider user base.       | Should  |
+
+
+Vragen naar Caroline
+* What is your current metodololy for quantification of aggregate? like
+    1. Get .lif file
+    2. Open Fiji
+    3. Open AggreCount
+    4. get results for CSV file (2d)
+
+* Wat is your 
