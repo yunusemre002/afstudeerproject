2c53b417f1578abdf7da19fc3303988d5e23a6f1
yunus <yedemir001@gmail.com>
2025-05-26 19:48:19 +0200

    more simple

--- codes_yunus/napari_3d_look.py
+++ codes_yunus/napari_3d_look.py
@@ -1,29 +1,20 @@
 import numpy as np
-from cellpose import models, core, io, plot
-from pathlib import Path
-from tqdm import trange
-import matplotlib.pyplot as plt
-from natsort import natsorted
-from skimage import measure, morphology
+from cellpose import io 
+from skimage import morphology
+from skimage.measure import regionprops
+from scipy.ndimage import label
+# from skimage.measure import label
+import napari
 
-file_name = f"/Users/demir/Documents/Hva_AI/afstudeerproject/codes_yunus/input/tif_deneme_1.tif"
-img = io.imread(file_name) # (9, 4, 1024, 1024)
+file_path = "/Users/demir/Documents/Hva_AI/afstudeerproject/E35_tif/20240312_CKR_Exp35_STHdhQ97HA_96h_+BafA_CCT1_635P_HA_580_A11_460L_1.tif"
+img = io.imread(file_path) # (9, 4, 1024, 1024)
 print(f'img       : {img.shape}')
 
-# NUCLEI
 img_nuclei = img[:, 0, :, :]
-# img_nuclei = img_nuclei[6, :, :]
-print(f'Nuclei    : {img_nuclei.shape}')
-
-# NUCLEI
 img_aggregate = img[:, 2, :, :]
-# img_aggregate = img_aggregate[6, :, :]
-print(f'Aggregate : {img_aggregate.shape}')
-
-# CELL BODY
 img_cellbody = img[:, 3, :, :]
-# img_cellbody = img_cellbody[6, :, :]
-print(f'Cell body : {img_cellbody.shape}')
+
+print(f'Nuclei    : {img_nuclei.shape} \nAggregate : {img_aggregate.shape} \nCell body : {img_cellbody.shape}')
 
 # Process aggregates (Channel 2)
 def process_aggregates(img):
@@ -37,77 +28,14 @@ def process_aggregates(img):
     return aggregate_masks
 
 aggregate_masks = process_aggregates(img)
-
 aggregate_stack = np.stack(aggregate_masks, axis=0)  # shape: (Z, Y, X)
-
-
-from scipy.ndimage import label
 labeled_aggregates, num_aggregates = label(aggregate_stack)
 
+# -------------------
 
-from skimage.measure import regionprops
-
-props = regionprops(labeled_aggregates)
-
-for i, region in enumerate(props):
-    print(f"Agregat {i+1}:")
-    print("  - Voxel sayısı (hacim):", region.area)
-    print("  - Ağırlık merkezi:", region.centroid)
-
-total_aggregates = len(props)
-total_volume = sum([r.area for r in props])
-average_volume = total_volume / total_aggregates if total_aggregates > 0 else 0
-
-import pandas as pd
-
-# df = pd.DataFrame([{
-#     'id': i+1,
-#     'volume': r.area,
-#     'centroid_z': r.centroid[0],
-#     'centroid_y': r.centroid[1],
-#     'centroid_x': r.centroid[2]
-# } for i, r in enumerate(props)])
-
-# df.to_csv("aggregates_summary.csv", index=False)
-
-
-import matplotlib.pyplot as plt
-from skimage.measure import label
-import napari
-import numpy as np
-
-from scipy.ndimage import label
-
-import napari
-import numpy as np
-from scipy.ndimage import label
-
-# Z, C, Y, X boyutundaki img üzerinden çalışıyoruz
-
-# Kanal 0: Nucleus (çekirdek), Kanal 1/3: Hücre, Kanal 2: Agregat
-nucleus = img[:, 0, :, :]
-cell_body = img[:, 3, :, :]  # ya da 1
-
-file_name_masked = f"/Users/demir/Documents/Hva_AI/afstudeerproject/codes_yunus/masksed_Cell.tif"
-img_masked = io.imread(file_name_masked)  # (Z, C, Y, X)
-print(f'img_masked       : {img_masked.shape}')
-
-
-aggregate_stack = np.stack(aggregate_masks, axis=0)
-labeled_aggregates, num_aggregates = label(aggregate_stack)
-
-# Napari ile 3D görselleştirme
+# Napari 3D 
 viewer = napari.Viewer(ndisplay=3)  # 3D gösterim için
-# viewer.add_image(nucleus, name="Nucleus", colormap="gray", blending="additive", scale=(1, 1, 1))
-viewer.add_image(nucleus, name="Nucleus", colormap="gray", scale=(1, 1, 1))
-viewer.add_image(cell_body, name="Cell Body", colormap="green", blending="additive", scale=(1, 1, 1))
+viewer.add_image(img_nuclei, name="Nucleus", colormap="gray", scale=(1, 1, 1))
+viewer.add_image(img_cellbody, name="Cell Body", colormap="green", blending="additive", scale=(1, 1, 1))
 viewer.add_labels(labeled_aggregates, name="Aggregates", opacity=0.6, scale=(1, 1, 1))
-
 napari.run()
-
-# # Napari ile görselleştir
-# viewer = napari.Viewer()
-# viewer.add_image(nucleus, name="Nucleus", colormap="gray", blending="additive")
-# viewer.add_image(cell_body, name="Cell Body", colormap="green", blending="additive")
-# viewer.add_labels(labeled_aggregates, name="Aggregates", opacity=0.6)
-# napari.run()
