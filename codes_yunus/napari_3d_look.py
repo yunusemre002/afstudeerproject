61d9c451a95a6cd3d74c5ffc0e1a36e61414e10c
yunus <yedemir001@gmail.com>
2025-05-26 20:26:18 +0200

    comments added

--- codes_yunus/napari_3d_look.py
+++ codes_yunus/napari_3d_look.py
@@ -1,10 +1,11 @@
 import numpy as np
 from cellpose import io 
 from skimage import morphology
-from skimage.measure import regionprops
 from scipy.ndimage import label
-# from skimage.measure import label
 import napari
+
+# --------------
+# FILE IMPORT
 
 file_path = "/Users/demir/Documents/Hva_AI/afstudeerproject/E35_tif/20240312_CKR_Exp35_STHdhQ97HA_96h_+BafA_CCT1_635P_HA_580_A11_460L_1.tif"
 img = io.imread(file_path) # (9, 4, 1024, 1024)
@@ -16,7 +17,10 @@ img_cellbody = img[:, 3, :, :]
 
 print(f'Nuclei    : {img_nuclei.shape} \nAggregate : {img_aggregate.shape} \nCell body : {img_cellbody.shape}')
 
-# Process aggregates (Channel 2)
+
+# -----------------------
+# THRESHOLD for AGGREGATE
+
 def process_aggregates(img):
     aggregate_masks = []
     for z_slice in range(img.shape[0]):
@@ -31,9 +35,19 @@ aggregate_masks = process_aggregates(img)
 aggregate_stack = np.stack(aggregate_masks, axis=0)  # shape: (Z, Y, X)
 labeled_aggregates, num_aggregates = label(aggregate_stack)
 
-# -------------------
 
-# Napari 3D 
+# -----------------------
+# NUCLEI
+
+
+
+
+
+
+
+# -------------------
+# NAPARI 3D 
+
 viewer = napari.Viewer(ndisplay=3)  # 3D gösterim için
 viewer.add_image(img_nuclei, name="Nucleus", colormap="gray", scale=(1, 1, 1))
 viewer.add_image(img_cellbody, name="Cell Body", colormap="green", blending="additive", scale=(1, 1, 1))
